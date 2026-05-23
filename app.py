1df37bcb6bf72f2c39d45f9667c37373b94f38c8
sharonjoseph0909 <shamashama2016@gmail.com>
2026-05-23 03:31:14 -0400

    fourth project push

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+import voice_recognition
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('foundyou.html')
+
+@app.route("/search", methods=["GET"])
+def search():
+    query = request.args.get("q")
+    return f"You searched for: {query}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
